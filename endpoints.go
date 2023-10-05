f8dc0d8e10047fc03c7eca5e11160724c3540b6a
Chirag Ghosh <cghosh828049@gmail.com>
2023-10-06 00:17:50 +0530

    feat: start project

---
+++ endpoints.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	HOMEPAGE_URL        = "https://erp.iitkgp.ac.in/IIT_ERP3/"
+	WELCOMEPAGE_URL     = "https://erp.iitkgp.ac.in/IIT_ERP3/welcome.jsp" // Only accessible when NOT logged in
+	LOGIN_URL           = "https://erp.iitkgp.ac.in/SSOAdministration/auth.htm"
+	SECRET_QUESTION_URL = "https://erp.iitkgp.ac.in/SSOAdministration/getSecurityQues.htm"
+	OTP_URL             = "https://erp.iitkgp.ac.in/SSOAdministration/getEmilOTP.htm" // blame ERP for the typo
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/metakgp/iitkgp-erp-login
+
+go 1.21.1
