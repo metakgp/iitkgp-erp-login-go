8c4c6978e388e594b06f2d3fa55a96b4676f0d5f
Shikhar Soni <shikharsonee@gmail.com>
2023-11-24 17:21:23 +0530

    fix: otp made mandatory feat: can be used as a package + many minor changes

--- go.mod
+++ go.mod
@@ -1,39 +1,32 @@
-module github.com/metakgp/iitkgp-erp-login-go
+module iitkgp_erp_login
 
 go 1.21.1
 
 require (
-	github.com/anaskhan96/soup v1.2.5
 	github.com/go-ping/ping v1.1.0
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
-	golang.org/x/oauth2 v0.13.0
-	golang.org/x/term v0.13.0
-	google.golang.org/api v0.149.0
+	golang.org/x/oauth2 v0.14.0
+	golang.org/x/term v0.14.0
+	google.golang.org/api v0.151.0
 )
 
 require (
 	cloud.google.com/go/compute v1.23.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
-	github.com/knadh/koanf v1.5.0 // indirect
-	github.com/knadh/koanf/providers/file v0.1.0 // indirect
-	github.com/mitchellh/copystructure v1.2.0 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sync v0.4.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231016165738-49dd2c1f3d0b // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
 	google.golang.org/grpc v1.59.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
--- iitkgp_erp_login.go
+++ iitkgp_erp_login.go
@@ -1,4 +1,4 @@
-package main
+package iitkgp_erp_login
 
 import (
 	"encoding/json"
@@ -15,15 +15,15 @@ import (
 
 	// "github.com/anaskhan96/soup"
 	"github.com/go-ping/ping"
-	"github.com/pkg/browser"
 	"golang.org/x/term"
 )
 
+const logging = true
+
 type LoginDetails struct {
-	user_id  string
-	password string
-	answer   string
-	// sessionToken string
+	user_id      string
+	password     string
+	answer       string
 	requestedUrl string
 	email_otp    string
 }
@@ -34,23 +34,7 @@ type ErpCreds struct {
 	SecurityQuestionsAnswers map[string]string `json:"answers"`
 }
 
-// func get_sessiontoken(client http.Client, logging bool) string {
-// 	res, err := client.Get(HOMEPAGE_URL)
-// 	check_error(err)
-// 	defer res.Body.Close()
-
-// 	body, err := io.ReadAll(res.Body)
-// 	check_error(err)
-
-// 	doc := soup.HTMLParse(string(body))
-// 	sessionToken := doc.Find("input", "id", "sessionToken").Attrs()["value"]
-// 	if logging {
-// 		log.Println("Generated sessionToken")
-// 	}
-// 	return sessionToken
-// }
-
-func input_creds(client http.Client, logging bool) LoginDetails {
+func input_creds(client *http.Client, logging bool) LoginDetails {
 	loginDetails := LoginDetails{
 		requestedUrl: HOMEPAGE_URL,
 	}
@@ -90,7 +74,7 @@ func input_creds(client http.Client, logging bool) LoginDetails {
 	return loginDetails
 }
 
-func get_secret_question(client http.Client, roll_number string, logging bool) string {
+func get_secret_question(client *http.Client, roll_number string, logging bool) string {
 	data := map[string][]string{
 		"user_id": {roll_number},
 	}
@@ -121,87 +105,82 @@ func is_otp_required() bool {
 	return pinger.Statistics().PacketsRecv != 1
 }
 
-func is_session_alive(client http.Client, logging bool) (bool, string) {
+func is_session_alive(client *http.Client, logging bool) (bool, string) {
+	if !is_file(".session") {
+		return false, ""
+	}
+
 	if logging {
+		log.Println("Found session file")
 		log.Println("Checking session validity...")
 	}
 
-	var ssoToken string
 	session_byte, err := os.ReadFile(".session")
 	check_error(err)
-	ssoToken = string(session_byte)
+	ssoToken := string(session_byte)
 
 	res, err := client.Get(HOMEPAGE_URL + "?" + ssoToken)
 	check_error(err)
 	defer res.Body.Close()
 
+	if logging {
+		if res.ContentLength != 4145 {
+			log.Println("Session valid")
+		} else {
+			log.Println("Session invalid")
+		}
+	}
+
 	return res.ContentLength != 4145, ssoToken
 }
 
-func Login(logging bool) {
+func ERPSession() (*http.Client, string) {
 	jar, err := cookiejar.New(nil)
 	check_error(err)
 	client := http.Client{Jar: jar}
 
-	if is_file(".session") {
-		if logging {
-			log.Println("Found session file")
-		}
-		is_session_alive, ssoToken := is_session_alive(client, logging)
+	var ssoToken string
+	var isSession bool
+	isSession, ssoToken = is_session_alive(&client, logging)
 
-		if is_session_alive {
+	if !isSession {
+		loginDetails := input_creds(&client, logging)
+
+		if true {
 			if logging {
-				log.Println("Session valid")
+				log.Println("OTP is required")
 			}
-			browser.OpenURL(HOMEPAGE_URL + "?" + ssoToken)
-			return
-		} else {
-			if logging {
-				log.Println("Session invalid!")
-			}
+			loginDetails.email_otp = fetch_otp(&client, loginDetails.user_id, logging)
 		}
+
+		data := url.Values{}
+		data.Set("user_id", loginDetails.user_id)
+		data.Set("password", loginDetails.password)
+		data.Set("answer", loginDetails.answer)
+		data.Set("requestedUrl", loginDetails.requestedUrl)
+		data.Set("email_otp", loginDetails.email_otp)
+
+		res, err := client.PostForm(LOGIN_URL, data)
+		check_error(err)
+		defer res.Body.Close()
+
+		log.Println("ERP login complete!")
+		body, err := io.ReadAll(res.Body)
+		check_error(err)
+
+		bodys := string(body)
+		i := strings.Index(bodys, "ssoToken")
+		ssoToken = bodys[strings.LastIndex(bodys[:i], "\"")+1 : strings.Index(bodys, "ssoToken")+strings.Index(bodys[i:], "\"")]
+
+		err = os.WriteFile(".session", []byte(ssoToken), 0666)
+		check_error(err)
+
 	}
 
-	loginDetails := input_creds(client, logging)
-
-	if is_otp_required() {
-		if logging {
-			log.Println("OTP is required")
-		}
-		loginDetails.email_otp = fetch_otp(&client, loginDetails.user_id, logging)
-	}
-
-	data := url.Values{}
-	data.Set("user_id", loginDetails.user_id)
-	data.Set("password", loginDetails.password)
-	data.Set("answer", loginDetails.answer)
-	data.Set("requestedUrl", loginDetails.requestedUrl)
-	data.Set("email_otp", loginDetails.email_otp)
-
-	res, err := client.PostForm(LOGIN_URL, data)
-	check_error(err)
-	defer res.Body.Close()
-
-	// sessionToken := res.Header["Set-Cookie"][0]
-
-	log.Println("ERP login complete!")
-
-	body, err := io.ReadAll(res.Body)
+	u, err := url.Parse("https://erp.iitkgp.ac.in/")
 	check_error(err)
 
-	bodys := string(body)
-	i := strings.Index(bodys, "ssoToken")
-	ssoToken := bodys[strings.LastIndex(bodys[:i], "\"")+1 : strings.Index(bodys, "ssoToken")+strings.Index(bodys[i:], "\"")]
+	client.Jar.SetCookies(u, []*http.Cookie{{Name: "ssoToken", Value: ssoToken[9:]}})
 
-	err = os.WriteFile(".session", []byte(ssoToken), 0666)
-	check_error(err)
-
-	browser.OpenURL(HOMEPAGE_URL + "?" + ssoToken)
-
-	// getTimetable(&client, ssoToken, sessionToken, "CS")
-
-}
-
-func main() {
-	Login(true)
+	return &client, ssoToken
 }
